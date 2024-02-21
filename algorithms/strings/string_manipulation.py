9511349a0b7c3b730a90d94d18d0e5dc646ce0ba
Ula <ula.malin35@gmail.com>
2024-02-21 11:41:04 +0100

    new module string_manipulation implemented with algorithms + tests

---
+++ algorithms/strings/string_manipulation.py
@@ -0,0 +1,61 @@
+from collections import Counter, OrderedDict
+
+
+def reverse_string(text: str) -> str:
+    """
+    Reverses a given string.
+
+    Parameters:
+        text (str): The string to be reversed.
+
+    Returns:
+        str: The reversed string.
+    """
+
+    backwards = []
+    items_length = len(text)
+    for i in range(items_length - 1, -1, -1):
+        backwards.append(text[i])
+    return ''.join(backwards)
+
+
+def string_compression(text: str) -> str:
+    """
+    Compresses a given string by representing the count of occurrences for each character.
+
+    Parameters:
+        text (str): The string to be compressed.
+
+    Returns:
+        str: The compressed string indicating the count of each character.
+    """
+
+    chars_counter = Counter(text)
+    ordered_chars_counter = OrderedDict.fromkeys(text)
+    return ''.join([c + str(chars_counter[c]) for c in ordered_chars_counter])
+
+
+def custom_join(words: list[str], separator: str = '') -> str:
+    """
+    Concatenates a list of strings using the specified separator.
+
+    Parameters:
+        words (list[str]): The list of strings to be joined.
+        separator (str): The string to be used as a separator between each element in the list. Default is ''.
+
+    Returns:
+        str: A string resulting from concatenating the elements of the list with the separator.
+
+    Raises:
+        ValueError: If any element in the 'words' list is not a string.
+    """
+
+    if not all(isinstance(word, str) for word in words):
+        raise ValueError("All elements must be strings")
+    result = ''
+    for i in range(len(words)):
+        if i == len(words) - 1:
+            result += words[i]
+        else:
+            result += (words[i] + separator)
+    return result
