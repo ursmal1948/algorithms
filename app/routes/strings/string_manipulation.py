e0fcece6d8a5b31f1fe659b28db7cf102ef98885
ursmal1948 <ula.malin35@gmail.com>
2024-07-14 00:22:56 +0200

    updated of response message and codes

--- app/routes/strings/string_manipulation.py
+++ app/routes/strings/string_manipulation.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.INFO)
 
 strings_manipulation_blueprint = Blueprint('strings_manipulation', __name__, url_prefix='/strings_manipulation')
 
+
 # todo wszystko zrobione ze string manipulation
 
 @strings_manipulation_blueprint.route('/reverse', methods=['POST'])
@@ -20,17 +21,19 @@ def reverse_():
     text = request.args.get('text', type=str, default='ABCD').strip()
 
     if not text:
-        return jsonify({'message': 'String is empty'}), 404
+        return jsonify({'message': 'Empty string provided'}), 400
 
     reversed_text = reverse(text)
     return jsonify({'Text reversed successfuly': reversed_text}), 201
+
+# jak nie podam queyr params w ogole to ABACDA,a jak podam pusty to wejdzie w warunek
 
 
 @strings_manipulation_blueprint.route('/compress', methods=['POST'])
 def compress_():
     text = request.args.get('text', type=str, default='ABACDA').strip()
     if not text:
-        return jsonify({'message': 'String is empty'}), 404
+        return jsonify({'message': 'Empty string provided'}), 400
     compressed_text = compress(text)
     return jsonify({'Text compressed successfuly': compressed_text}), 201
 
@@ -39,7 +42,8 @@ def compress_():
 def custom_join_():
     text = request.args.get('text', type=str, default='ABACDA').strip()
     if not text:
-        return jsonify({'message': 'String is empty'}), 404
+        return jsonify({'message': 'Empty string provided'}), 400
+
     sep = request.args.get('sep', type=str, default='#')
     text_items = list(text)
     new_text = custom_join(text_items, sep)
@@ -52,11 +56,11 @@ def custom_join_():
 def transform(text: str):
     stripped_text = text.strip()
     if not stripped_text:
-        return jsonify({'message': 'String is empty'}), 404
-    case = request.args.get('case', type=str, default='lower')
+        return jsonify({'message': 'Empty string provided'}), 400
+    case = request.args.get('case', type=str)
 
     if case not in ['lower', 'upper']:
-        return jsonify({'message': 'Case is invalid'}), 404
+        return jsonify({'message': 'Case is invalid'}), 400
 
     if case == 'lower':
         return jsonify({'Text lowercased successfuly': lower(stripped_text)}), 201
