6fd327af25d055e0d954bad4cd38aa8431919722
ursmal1948 <ula.malin35@gmail.com>
2024-07-17 12:30:57 +0200

    All routes for ciphers module implemented

--- app/routes/ciphers.py
+++ app/routes/ciphers.py
@@ -2,11 +2,9 @@ from app.algohub.algorithms.ciphers.caesar import CaesarCipher
 from app.algohub.algorithms.ciphers.morse import MorseCode
 from app.algohub.algorithms.ciphers.vigenere import VigenereCipher
 from app.algohub.algorithms.ciphers.caesar import CaesarCipher
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, Flask
+import re
 
-from flask_restful import reqparse
-
-from flask import Flask
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +13,7 @@ logging.basicConfig(level=logging.INFO)
 def configure_ciphers(app: Flask) -> None:
     caesar_cipher = CaesarCipher()
     vigenere_cipher = VigenereCipher()
+    morse_cipher = MorseCode()
 
     @app.route('/ciphers/caesar-ecnryption', methods=['POST'])
     def encrypt_caesar():
@@ -63,4 +62,20 @@ def configure_ciphers(app: Flask) -> None:
         decrypted_text = vigenere_cipher.decrypt(text)
         return jsonify({'Decrypted_text': f'{decrypted_text}'}), 200
 
+    @app.route('/ciphers/morse-encryption', methods=['POST'])
+    def encrypt_morse():
+        text = request.args.get('text', type=str)
+        if not text:
+            return jsonify({'message': 'No text provided'}), 400
 
+        decrypted_text = morse_cipher.encrypt(text)
+        return jsonify({'Encrypted_text': f'{decrypted_text}'}), 200
+
+    @app.route('/ciphers/morse-decryption', methods=['POST'])
+    def decrypt_morse():
+        text = request.args.get('text', type=str)
+
+        if not re.match(r'^[\\|.-]+$', text):
+            return jsonify({'message': 'Text must consist of dots and dashes and vertical bars '}), 400
+        decrypted_text = morse_cipher.decrypt(text)
+        return jsonify({'Decrypted_text': f'{decrypted_text}'}), 200
