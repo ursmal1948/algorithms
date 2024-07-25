6160724f540d7528b0cb7221534384ddf3f20dd1
ursmal1948 <ula.malin35@gmail.com>
2024-07-25 21:35:16 +0200

    Tests for string manipulation and string analysis endpoints implemented

---
+++ tests/routes/strings/test_string_analysis_endpoints.py
@@ -0,0 +1,65 @@
+from string import ascii_lowercase
+
+# import json
+import pytest
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestStringAnalysisEndpoints:
+    # palindrome_path = '/api/algorithms/strings/string-analysis/palindrome'
+    # anagram_path = '/api/algorithms/strings/string-analysis/anagrams'
+    # pangram_path = '/api/algorithms/strings/string-analysis/pangram'
+
+    @pytest.mark.parametrize('payload_arg, expected_result', [
+        ('abcd', False),
+        ('ABA', True),
+        ('', False)
+
+    ])
+    def test_palindrome_route(self, client, payload_arg, expected_result, palindrome_path):
+        json_payload = {
+            'text': payload_arg,
+        }
+        response = client.post(palindrome_path, json=json_payload)
+
+        if response.status_code == 200:
+            assert response.json['palindrome'] == expected_result
+        else:
+            assert response.status_code == 500
+            assert (response.json['message'] ==
+                    "('Error validating against schema', [<ValidationError: \"'' should be non-empty\">])"
+                    )
+
+    @pytest.mark.parametrize('potential_pangram, expected_result', [
+        (ascii_lowercase, True),
+        ('AB', False),
+        ('', False)
+    ])
+    def test_pangram_route(self, client, potential_pangram, expected_result, pangram_path):
+        json_payload = {
+            'text': potential_pangram
+        }
+        response = client.post(pangram_path, json=json_payload)
+        if response.status_code == 500:
+            assert (response.json['message'] ==
+                    "('Error validating against schema', [<ValidationError: \"'' should be non-empty\">])"
+                    )
+        else:
+            assert response.json['pangram'] == expected_result
+            assert response.status_code == 200
+
+    @pytest.mark.parametrize('payload_arg1, payload_arg2, expected_result', [
+        ['abc', 'cba', True],
+        ['ab', 'bd', False]
+    ])
+    def test_anagrams_route(self, client, payload_arg1, payload_arg2, expected_result, anagrams_path):
+        json_payload = {
+            'text1': payload_arg1,
+            'text2': payload_arg2,
+        }
+        response = client.post(anagrams_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['anagrams'] == expected_result
---
+++ tests/routes/strings/test_string_manipulation_endpoints.py
@@ -0,0 +1,64 @@
+import pytest
+
+
+class TestStringManipulationEndpoints:
+    # join_path = 'api/algorithms/strings/string-manipulation/join'
+    # transformation_path = 'api/algorithms/strings/string-manipulation/transform'
+    # compress_path = 'api/algorithms/strings/string-manipulation/compress'
+    # reverse_path = 'api/algorithms/strings/string-manipulation/reverse'
+
+    def test_custom_join_with_default_separator_route(self, client, custom_join_path):
+        json_payload = {'text': 'ABC'}
+        response = client.post(custom_join_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['joined'] == 'A-B-C'
+
+    def test_custom_join_with_custom_separator_route(self, client, custom_join_path):
+        json_payload = {'text': 'ABC', 'separator': '*'}
+        response = client.post(custom_join_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['joined'] == 'A*B*C'
+
+    @pytest.mark.parametrize('text, case, expected_result', [
+        ('abc', 'upper', 'ABC'),
+        ('DEF', 'lower', 'def')
+    ])
+    def test_lower_upper_transformation_route(self, client, text, case, expected_result, transformation_path):
+        json_payload = {'text': text, 'case': case}
+        response = client.post(transformation_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['transformed'] == expected_result
+
+    def test_transformation_route_invalid_case(self, client, transformation_path):
+        json_payload = {'text': 'ABC', 'case': 'invalid_case'}
+        response = client.post(transformation_path, json=json_payload)
+        assert response.status_code == 400
+        assert response.json['message'] == 'invalid case'
+
+    def test_transformation_route_with_empty_string(self, client, transformation_path):
+        json_payload = {'text': '', 'case': 'lower'}
+        response = client.post(transformation_path, json=json_payload)
+        assert response.status_code == 500
+        assert (response.json['message'] ==
+                "('Error validating against schema', [<ValidationError: \"'' should be non-empty\">])"
+                )
+
+    def test_compress_route(self, client, compress_path):
+        json_payload = {'text': 'ABBBAC'}
+        response = client.post(compress_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['compressed'] == 'A2B3C1'
+
+    def test_compress_route_with_empty_string(self, client, compress_path):
+        json_payload = {'text': ''}
+        response = client.post(compress_path, json=json_payload)
+        assert response.status_code == 500
+        assert (response.json['message'] ==
+                "('Error validating against schema', [<ValidationError: \"'' should be non-empty\">])"
+                )
+
+    def test_reverse_route(self, client, reverse_path):
+        json_payload = {'text': 'ABC'}
+        response = client.post(reverse_path, json=json_payload)
+        assert response.status_code == 200
+        assert response.json['reversed'] == 'CBA'
