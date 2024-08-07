aa368f2bbd5046f8e985da268e8db400e181d31c
ursmal1948 <ula.malin35@gmail.com>
2024-08-07 23:15:04 +0200

    utility functions created that are used in routes,

---
+++ app/algohub/utility_functions.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+
+def create_polynomial_function(coefficients: list[int]) -> Callable[[int | float], int | float]:
+    def polynomial_function(x):
+        result = 0
+        for index, coeff in enumerate(coefficients):
+            result += coeff * (x ** (len(coefficients) - 1 - index))
+        return result
+
+    return polynomial_function
+
+
+def execute_function_from_string(expression: str, x_value: int) -> int | float:
+    try:
+        x = x_value
+        return eval(expression)
+    except Exception as e:
+        raise ValueError(f"Error evaluating expression: {e}")
