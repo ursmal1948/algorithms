b056dabbdda16a668463bbb35e35dcff1df52562
ursmal1948 <ula.malin35@gmail.com>
2024-08-13 02:18:57 +0200

    added routes for checking for substring, subsequence and containing duplicates

--- app/routes/strings/analysis.py
+++ app/routes/strings/analysis.py
@@ -4,12 +4,20 @@ from app.algohub.algorithms.strings.analysis import (
     is_palindrome,
     is_potential_palindrome,
     is_substring,
-    is_subsequence
+    is_subsequence,
+    count_substring_occurences,
+    contains_duplicates
+
 )
 from flask import request, jsonify, Blueprint, Flask, Response
 from flask_json_schema import JsonSchema
 
-from app.routes.schemas import text_schema, anagrams_schema
+from app.routes.schemas import (
+    text_schema,
+    anagrams_schema,
+    text_and_subsequence,
+    strings_list_schema
+)
 
 schema2 = JsonSchema()
 strings_analysis_blueprint = Blueprint('string_analysis', __name__,
@@ -23,6 +31,15 @@ def handle_is_palindrome() -> Response:
     text = data['text']
     result = is_palindrome(text)
     return jsonify({'palindrome': result}), 200
+
+
+@strings_analysis_blueprint.route('/potential-palindrome', methods=['POST'])
+@schema2.validate(text_schema)
+def handle_is_potential_palindrome() -> Response:
+    data = request.json
+    text = data['text']
+    result = is_potential_palindrome(text)
+    return jsonify({'potential palindrome': result}), 200
 
 
 @strings_analysis_blueprint.route('/pangram', methods=['POST'])
@@ -42,3 +59,42 @@ def handle_are_anagrams() -> Response:
     text2 = data['text2']
     result = are_anagrams(text1, text2)
     return jsonify({'anagrams': result}), 200
+
+
+@strings_analysis_blueprint.route('/subsequence', methods=['POST'])
+@schema2.validate(text_and_subsequence)
+def handle_is_subsequence() -> Response:
+    data = request.json
+    text = data['text']
+    subsequence = data['sub_text']
+    result = is_subsequence(text, subsequence)
+    return jsonify({'subsequence': result}), 200
+
+
+@strings_analysis_blueprint.route('/substring', methods=['POST'])
+@schema2.validate(text_and_subsequence)
+def handle_is_substring() -> Response:
+    data = request.json
+    text = data['text']
+    substring = data['sub_text']
+    result = is_substring(text, substring)
+    return jsonify({'substring': result}), 200
+
+
+@strings_analysis_blueprint.route('/substring-occurences', methods=['POST'])
+@schema2.validate(text_and_subsequence)
+def handle_count_substring_occurences() -> Response:
+    data = request.json
+    text = data['text']
+    substring = data['sub_text']
+    result = count_substring_occurences(text, substring)
+    return jsonify({'substring occurences': result}), 200
+
+
+@strings_analysis_blueprint.route('/duplicates', methods=['POST'])
+@schema2.validate(strings_list_schema)
+def handle_contains_duplicates() -> Response:
+    data = request.json
+    items = data["items"]
+    result = contains_duplicates(items)
+    return jsonify({'duplicates': result}), 200
